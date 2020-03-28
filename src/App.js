1f44a36170c9c48a8e304027d73bf7c4ac3235ef
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-28 01:07:32 -0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from  './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter +1);
+  }
+
   return (
-    <Header>
-      Semana Omnistack
-    </Header>
+    <div>
+      <Header>
+        Contador: { counter }
+      </Header>
+      <button onClick={increment} >Adicionar</button>
+    </div>
   );
 }
 
