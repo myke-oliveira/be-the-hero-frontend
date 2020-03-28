645532a252c5e8d2a74bde80e8cd110c135fc4d7
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-28 00:55:53 -0300

    Props and Children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Header from  './Header';
+
 function App() {
   return (
-    <h1>Hello Myke</h1>
+    <Header>
+      Semana Omnistack
+    </Header>
   );
 }
 
